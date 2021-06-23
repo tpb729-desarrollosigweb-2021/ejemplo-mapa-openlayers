bb6c874d8696eba2a3b75f4db40013e5388451f9
Manuel Vargas <mfvargas@gmail.com>
2021-06-23 14:51:00 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
+// Mapa
 var mapa = new ol.Map({
   target: 'mapa',
   layers: [
     new ol.layer.Tile({
+      source: new ol.source.Stamen({layer: 'watercolor'})
+    }),
+    new ol.layer.Tile({
       source: new ol.source.OSM()
-    })
+    }),
   ],
   view: new ol.View({
     center: ol.proj.fromLonLat([-84.1, 9.5]),
     zoom: 8
   })
 });
+
+
+// Controles
+mapa.addControl(new ol.control.ZoomSlider());
+mapa.addControl(new ol.control.MousePosition());
+mapa.addControl(new ol.control.ScaleLine());
